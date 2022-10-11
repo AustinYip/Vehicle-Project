900964a59f21b87413b291a259e6669cef42c1e2
AustinYip <101162675+AustinYip@users.noreply.github.com>
2022-10-11 14:50:48 -0400

    Add files via upload

---
+++ Superman.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Superman here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Superman extends Vehicle
+{
+    /**
+     * Act - do whatever the Superman wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Superman(VehicleSpawner origin) {
+        super(origin); // call the superclass' constructor
+        maxSpeed = 1.5 + ((Math.random() * 30)/2);
+        speed = maxSpeed;
+        yOffset = 0;
+    }
+    public void act()
+    {
+        drive(); 
+        checkHitPedestrian();
+        if (checkEdge()){
+            getWorld().removeObject(this);
+        }
+    }
+    public boolean checkHitPedestrian () {
+        Pedestrian p = (Pedestrian)getOneObjectAtOffset((int)speed + getImage().getWidth()/2, 0, Pedestrian.class);
+        
+        if (p != null){
+            p.knockDown();
+            return true;
+        }
+        return false;
+    }
+}
